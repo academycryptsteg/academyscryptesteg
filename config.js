ce1478c586f48f182cfb2535cf37eb95c9c3c4a8
academycryptsteg <academy.cryptsteg@gmail.com>
2026-05-15 15:23:16 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,3 @@
-// CONFIGURACIÓN DE SUPABASE
-// 1) Crear el proyecto en Supabase.
-// 2) Ejecutar el archivo supabase.sql en el SQL Editor.
-// 3) Copiar aquí la Project URL y la anon public key.
-// Si dejas estos campos vacíos, el formulario mostrará el mensaje pero no guardará en base.
-
 window.CRYPTSTEG_CONFIG = {
   SUPABASE_URL: "https://qhybeaqnowwvbgsobtos.supabase.co",
   SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFoeWJlYXFub3d3dmJnc29idG9zIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg4NTY3OTAsImV4cCI6MjA5NDQzMjc5MH0.nlBmKu2IMn8ubv-Z3r_8Fa6hPcHPIgJam3y-CLhPCDI"
